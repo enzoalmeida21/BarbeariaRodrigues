b8d286ac3a52c27af74fbf5f1ab34f0b25282740
Enzo Almeida <77026794+enzoalmeida21@users.noreply.github.com>
2023-11-22 21:56:15 -0300

    commit inicial

---
+++ src/main/java/com/barbeariarodrigues/barbeariaRodriguesApp/BarbeariaRodriguesAppApplication.java
@@ -0,0 +1,13 @@
+package com.barbeariarodrigues.barbeariaRodriguesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaRodriguesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaRodriguesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbeariarodrigues/barbeariaRodriguesApp/BarbeariaRodriguesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbeariarodrigues.barbeariaRodriguesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaRodriguesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
